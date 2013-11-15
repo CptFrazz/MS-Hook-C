35d6054c905b2a75269ab3985d1bbaac71129b70
Grant Douglas <hexploitable@gmail.com>
2013-11-15 19:16:57 +0000

    Prettifying
    Made the console output easy on the eyes.

--- MethodScanner.c
+++ MethodScanner.c
@@ -6,7 +6,7 @@
  *              I.e. when you can't use MSFindSymbol().
  *
  * Usage:       Open app in disassembler, grab first 16 bytes (might need to tweak this)
- *              of your target method. Use this as signature. Define sigsize as number of bytes you're using.
+ *              of your target method. Use this as signature.
  *
  */
 #include <sys/types.h>
@@ -20,6 +20,22 @@
 #include <string.h>
 #include <limits.h>
 #include <errno.h>
+
+#define red   "\033[1;31m"        /* 0 -> normal ;  31 -> red */
+#define redU   "\033[4;31m"
+#define cyan  "\033[0;36m"        /* 1 -> bold ;  36 -> cyan */
+#define cyanU "\033[4;36m"
+#define green "\033[0;32m"        /* 4 -> underline ;  32 -> green */
+#define yellow "\033[0;33m"
+#define yellowU "\033[4;33m"
+#define blue  "\033[0;34m"        /* 9 -> strike ;  34 -> blue */
+#define blueU "\033[4;34m"
+#define black  "\033[0;30m"
+#define brown  "\033[0;33m"
+#define magenta  "\033[0;35m"
+#define gray  "\033[0;37m"
+#define uline   "\033[4;0m"
+#define none   "\033[0m"        /* to flush the previous property */
 
 int pid = 0;
 int g_pid = 0;
@@ -36,34 +52,31 @@ mach_vm_address_t *scanMem(int pid, mach_vm_address_t addr, mach_msg_type_number
     kern_return_t kr_val;
     pointer_t memStart;
     uint32_t sz;
-    int len = needleLen;
-    unsigned char buffer[len];
-    int i = 0;
+    unsigned char buffer[needleLen];
 
     while (bytesRead < size)
     {
-        if ((kr_val = vm_read(t, addr, len, &memStart, &sz)) == KERN_SUCCESS)
+        if ((kr_val = vm_read(t, addr, needleLen, &memStart, &sz)) == KERN_SUCCESS)
         {
             memcpy(buffer, (const void *)memStart, sz);
-            //printf("Size of read: %d, Size of needle: %d\n", sizeof(buffer), sizeof(nBuffer));
-            if (memcmp(buffer, nBuffer, len) == 0)
+            if (memcmp(buffer, nBuffer, needleLen) == 0)
             {
                 fflush(stdout);
                 return (unsigned long long *)addr;
             }
             else
-                printf("[-] %p ---> vm_read()\r", addr);
+                printf("[%s-%s] %s%p%s ---> vm_read()\r", red, none, redU, addr, none);
             fflush(stdout);
         }
         else
         {
-            printf("[-] %p ---> vm_read().\r", addr);
+            printf("[%s-%s] %s%p%s ---> vm_read()\r", red, none, redU, addr, none);
             fflush(stdout);
         }
         addr += sizeof(unsigned char);
         bytesRead += sizeof(unsigned char);
     }
-    printf("[i] Scanning ended without a match.\r\n");
+    printf("[%si%s] Scanning ended without a match.\r\n", yellow, none);
     fflush(stdout);
     return NULL;
 }
@@ -81,7 +94,6 @@ unsigned int *getMemRegions(task_t task, vm_address_t address)
     count = VM_REGION_BASIC_INFO_COUNT_64;
     int regionCount = 0;
     int flag = 0;
-
 
     while (flag == 0)
     {
@@ -101,7 +113,7 @@ unsigned int *getMemRegions(task_t task, vm_address_t address)
             flag = 1;
     }
     lastRegionEnd = address;
-    printf("[i] Region to scan: %p - %p\n", firstRegionBegin, lastRegionEnd);
+    printf("[%si%s] Proc Space: %s%p%s - %s%p%s\n", yellow, none, yellowU, firstRegionBegin, none, blueU, lastRegionEnd, none);
     unsigned int *ptrToFunc = (unsigned int *)scanMem(pid, firstRegionBegin, fullSize);
     return ptrToFunc;
 }
@@ -112,7 +124,6 @@ int main(int argc, char** argv) {
     mach_port_t task;
     mach_vm_address_t addr = 1;
 
-
     if (argc >= 3)
     {
         pid = atoi(argv[1]);
@@ -120,17 +131,9 @@ int main(int argc, char** argv) {
         rc = task_for_pid(mach_task_self(), pid, &task);
         if (rc)
         {
-            fprintf(stderr, "[-] task_for_pid() failed, error %d - %s", rc, mach_error_string(rc));
+            fprintf(stderr, "[%s-%s] task_for_pid() failed, error %d - %s%s", red, none, rc, red, mach_error_string(rc), none);
             exit(1);
         }
-
-        FILE *fr;
-        fr = fopen(argv[2], "rb");
-        unsigned char buf[256];
-        long int cnt = 0;
-        while ((cnt = (long)fread(buf, sizeof(unsigned char), 16, fr))>0)
-            nBuffer = buf;
-        fclose(fr);
 
         FILE *f = fopen(argv[2], "rb");
         if (f)
@@ -139,16 +142,25 @@ int main(int argc, char** argv) {
             needleLen = ftell(f);
             fclose(f);
         }
-        //needleLen = atoi(argv[3]);
+        
+        unsigned char buf[needleLen+1];
+        FILE *fr;
+        fr = fopen(argv[2], "rb");
+        long int cnt = 0;
+        while ((cnt = (long)fread(buf, sizeof(unsigned char), 16, fr))>0)
+            nBuffer = buf;
+        fclose(fr);
 
-        printf("[+] PID: %d\n[i] Task: %d\n[+] Needle Length: \"%d\"\n", pid, task, needleLen);
+        printf("[%s+%s] PID: %s%d%s\n", green, none, blueU, pid, none);
+        printf("[%si%s] Task: %s%d%s\n", yellow, none, blueU, task, none);
+        printf("[%s+%s] Needle Length: %s%d%s %sbytes%s\n", green, none, blue, needleLen, none, blueU, none);
         unsigned int *sym = getMemRegions(task, addr);
         if (sym != NULL)
-            printf("\n\n\t[$] Located target function ---> %p\n\n", sym);
+            printf("\n\n[%s$%s] Located target function ---> %s%p%s\n\n", cyan, none, cyanU, sym, none);
         else
-            printf("[-] Didn\'t find the function.\n");
+            printf("[%s-%s] Didn\'t find the function.\n", red, none);
     }
     else
-        fprintf(stderr, "[i] Usage: %s <pid> <Path to file containing needle>\n", argv[0]);
+        fprintf(stderr, "[%s-%s] Usage: %s <pid> <Path to file containing needle>\n", red, none, argv[0]);
     return 0;
 }
